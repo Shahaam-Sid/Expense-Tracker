eac1181ea43b32131519d881e89ad1139c2169ec
Shahaam-Sid <muhammadgc821@gmail.com>
2025-09-01 09:44:00 +0500

    Added getter and setter for Expense

--- Expense.py
+++ Expense.py
@@ -2,38 +2,68 @@ from datetime import datetime as d
 
 class Expense:
     def __init__(self, title, cost, note):
-        if not isinstance(title, str):
-            raise TypeError("Title must be String")
-        
-        if not isinstance(cost, (int, float)):
-            raise TypeError("Cost must be an Integer or Float value")
-        
-        if not isinstance(note, str):
-            raise TypeError("Note must be a String")
-        
-        if len(title) == 0 or title.isspace() or title.isdigit():
-            raise ValueError("Title must contains Words")
-        
-        if cost <= 0:
-            raise ValueError("Cost cannot be 0 or less")
-        
-        if len(note) == 0 or note.isspace() or note.isdigit():
-            raise ValueError("Title must contains Words")
-        
+
         self.title = title
         self.cost = cost
         self.note = note
         
         dd = d.today().strftime("%d-%b-%Y")
         
-        self.date = dd
+        self._date = dd
         
         tt = d.now().strftime("%H:%M")
         
-        self.time = tt
+        self._time = tt
+        
+    @property
+    def title(self):
+        return self._title
+    
+    @property
+    def cost(self):
+        return self._cost
+    
+    @property
+    def note(self):
+        return self._note
+    
+    @property
+    def date(self):
+        return self._date
+    
+    @property
+    def time(self):
+        return self._time
+    
+    @title.setter
+    def title(self, value):
+        if not isinstance(value, str):
+            raise TypeError("Title must be String")
+        
+        if len(value) == 0 or value.isspace() or value.isdigit():
+            raise ValueError("Title must contains Words")
+        
+        self._title = value
+        
+    @cost.setter
+    def cost(self, value):
+        if not isinstance(value, (int, float)):
+            raise TypeError("Cost must be an Integer or Float value")
+        
+        if value <= 0:
+            raise ValueError("Cost cannot be 0 or less")
+        
+        self._cost = value
+        
+    @note.setter
+    def note(self, value):
+        if not isinstance(value, str):
+            raise TypeError("Note must be a String")
+        
+        if len(value) == 0 or value.isspace() or value.isdigit():
+            raise ValueError("Note must contains Words")
+        
+        self._note = value
         
     def __str__(self):
         return f"{self.title}: Rs.{self.cost}, Time/Date: {self.time}/{self.date} note: {self.note}"
-        
-x = Expense("Test", 29, "Testing the class")
-print(x)
