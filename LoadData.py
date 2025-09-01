379868439c42fabb72325bb78ecf44989d7c1891
Shahaam-Sid <muhammadgc821@gmail.com>
2025-09-01 11:14:11 +0500

    Added extract_from_json

--- LoadData.py
+++ LoadData.py
@@ -1,6 +1,7 @@
 import json
 from Expense import Expense
 from datetime import datetime as d
+import calendar
 
 class LoadData:
     
@@ -53,3 +54,46 @@ class LoadData:
             
         with open(self.filepath, 'w') as file:
             json.dump(self._expenses, file, indent=4)
+            
+            
+    @classmethod
+    def extract_from_json(cls, month = 0, year = 0):
+        if not isinstance(month, int):
+            raise TypeError("Month can only be a Integer Value")
+        
+        if not isinstance(year, int):
+            raise TypeError("Year can only be an Ineteger Value")
+        
+        if year < 0:
+            raise ValueError("Year Cannot be a negative Value")
+        
+        if month < 0 or month > 12:
+            raise ValueError("Month Cannit be less then zero and more the 12")
+        
+        try:
+            with open(cls.filepath, 'r') as file:
+                    expenses = json.load(file)
+                    
+        except (FileNotFoundError, json.JSONDecodeError):
+            print("No Data Found")
+            return {}
+        
+        if year == 0:
+            return expenses
+    
+        year_str = str(year)
+        if year_str not in expenses:
+            print(f"No data for year {year}")
+            return {}
+    
+        if month == 0:
+            return expenses[year_str]
+    
+        month_str = calendar.month_abbr[month]
+        if month_str not in expenses[year_str]:
+            print(f"No data for {month_str} {year}")
+            return {}
+    
+        return expenses[year_str][month_str]
+            
+        
---
+++ main.py
@@ -0,0 +1,4 @@
+from Expense import Expense
+from LoadData import LoadData
+
+print(LoadData.extract_from_json(year=2025))
